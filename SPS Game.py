454a9fde60c3b43bf256f753faaa1bc0910062b8
Mohamed Ukkasha Sathali K K <sathalisathali250@gmail.com>
2026-01-18 16:09:03 +0530

    This file consists of the coding of childhood game

---
+++ SPS Game.py
@@ -0,0 +1,39 @@
+from random import*
+st=["stone","paper","scissor"]
+ch=1
+sc=0
+sys=0
+
+while ch<=5:
+    user=input("User choice:")
+    comp=choice(st)
+    print("Computer Choice:",comp)
+    if user==comp:
+        sc+=0
+        sys+=0
+    elif user=="stone":
+        if comp=="scissor":
+            sc+=1
+        else:
+            sys+=1
+    elif user=='paper':
+        if comp=='stone':
+            sc+=1
+        else:
+            sys+=1
+    elif user=='scissor':
+        if comp=='paper':
+            sc+=1
+        else:
+            sys+=1
+    ch+=1
+print("Computer score:",sys)
+print("user score:",sc)
+
+if sys==sc:
+    print("Score Level")
+elif sys>sc:
+    print("Computer won")
+else:
+    print("User won")
+
